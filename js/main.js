f96e3a5f1f26cf9eef572c2ee6a077d988905d12
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-07 22:33:42 +0100

    FORM card with v model

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue 
+
+    createApp({
+        data() {
+            return{
+                cvc : "",
+                cardName : "",
+                cardNumber : ""
+
+            }
+        },
+        methods:{
+          formatCardNumber(){
+            
+          }
+        },
+        mounted(){
+         
+        }
+
+    }).mount("#webapp")
+
+    
