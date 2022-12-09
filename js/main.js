0d27cbf50eb283593e8bef6f05f54dd7dde8e133
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-09 22:12:15 +0100

    validations

--- js/main.js
+++ js/main.js
@@ -9,32 +9,41 @@ const { createApp } = Vue
                 monthExp : "",
                 yearExp : "",
                 cardAdded : false,
-                error : false,
+                errorHolder : false,
+                errorNumber : false,
+                field : 0,
                
             }
         },
-        computed: {
-            // formatCardNumber(){
-            //   return this.cardNumber ? this.cardNumber.match(/.{1,4}/g).join(' ') : '';
-            // } 
-          },
-        
         methods:{
             cardHolderFormatted(){
                 this.nameFormatted = this.cardName.toUpperCase()
-                var letters = /^[a-zA-Z ]+$/
+                let letters = /^[a-zA-Z ]+$/
                 if(this.nameFormatted.match(letters)){
-                    this.error = false
+                    this.errorHolder = false
                 }else{
-                    this.error = true
+                    this.errorHolder = true
                 }
             },
             formatCard() {
+                let numbers = /^[0-9]+$/;
+                if (this.card.match(numbers)){
+                    this.errorNumber = false
+                }else{
+                    this.errorNumber = true
+                }
                 let nn = this.card;
                 (nn.length - (nn.split(" ").length - 1)) % 4 === 0 ? this.card += ' ' : ' '
             },
             addCard(){
-                this.cardAdded = true
+                if (( this.errorHolder === false) && ( this.errorNumber === false) && ( this.nameFormatted !== "") && ( this.card !== "") && (this.monthExp !== "") && (this.yearExp !== "")){
+                    this.cardAdded = true
+                }else{
+                    this.cardAdded = false
+                    this.field = 1
+                }
+ 
+                
             },
             refreshPage(){
                 location.reload()
@@ -51,17 +60,3 @@ const { createApp } = Vue
 
     }).mount("#webapp")
 
-    // ccformat() {
-    //   var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
-    //   var matches = v.match(/\d{4,16}/g);
-    //   var match = matches && matches[0] || ''
-    //   var parts = []
-    //   for (i=0, len=match.length; i<len; i+=4) {
-    //     parts.push(match.substring(i, i+4))
-    //   }
-    //   if (parts.length) {
-    //     return parts.join(' ')
-    //   } else {
-    //     return value
-    //   }
-    // }
